770b241efefbacaaec7e3595c299420201f65c0d
cladams0203 <cladams0203@gmail.com>
2019-12-03 23:05:54 -0500

    testing the proxy

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const userDb = require('./users/userDb')
 
 const server = express();
+
 server.use(logger)
+
+server.get('/:id',validateUserId, (req, res) => {
+
+})
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
@@ -13,5 +20,10 @@ function logger(req, res, next) {
   
   next()
 }
+function validateUserId(req, res, next) {
+  userDb.getById(req.params.id)
+    .then(user => console.log(user))
+  next();
+}
 
-module.exports = server;
+module.exports = server, validateUserId;
