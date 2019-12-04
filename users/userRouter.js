90e5e44cf9f05633125873533cbba42cd549a225
cladams0203 <cladams0203@gmail.com>
2019-12-04 13:29:11 -0500

    finished user endpoints

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,36 +2,90 @@ const express = require('express');
 
 const router = express.Router();
 const userDb = require('./userDb')
+const postDb = require('../posts/postDb')
+
 
 
 
 router.post('/', validateUser, (req, res) => {
   // do your magic!
+  userDb.insert(req.body)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      res.status(500).json({message: 'failed to add user'})
+    })
 });
 
 router.post('/:id/posts', [validateUserId, validatePost], (req, res) => {
   // do your magic!
+  !req.body.user_id ?
+  res.status(400).json({message: 'require user id field in body'}) :
+  postDb.insert(req.body)
+    .then(userPost => {
+      res.status(200).json(userPost)
+    })
+    .catch(err => {
+      res.status(500).json({message: 'unable to add post to user'})
+    })
+  
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  userDb.get()
+    .then(users => {
+      res.status(200).json(users)
+    })
+    .catch(err => {
+      res.status(500).json({message: 'page could not load'})
+    })
 });
 
 router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
+  userDb.getById(req.user.id)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(err => {
+      res.status(500).json({message: 'could not load page'})
+    })
   
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
+  userDb.getUserPosts(req.user.id)
+    .then(userPost => {
+      res.status(200).json(userPost)
+    })
+    .catch(err => {
+      res.status(500).json({message: 'could nopt load page'})
+    })
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
   // do your magic!
+  userDb.remove(req.user.id)
+    .then(user => {
+      res.status(200).json(user)
+    })
+    .catch(err => {
+      res.status(500).json({message: 'could not load page'})
+    })
 });
 
-router.put('/:id', validateUserId, (req, res) => {
+router.put('/:id', [validateUserId, validateUser], (req, res) => {
   // do your magic!
+  userDb.update(req.user.id, req.body)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      res.status(500).json({message: 'unable to update user information'})
+    })
 });
 
 //custom middleware
