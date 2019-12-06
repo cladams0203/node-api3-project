f64568c9bf8d3309f7491c6f5cf59e83dda8c364
cladams0203 <cladams0203@gmail.com>
2019-12-06 12:44:35 -0500

    deploying on heropku

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // code away!
+require('dotenv').config()
+
 const server = require('./server')
 
-server.listen(5000, () => {
-    console.log('running on 5000')
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`running on ${port}`)
 })
